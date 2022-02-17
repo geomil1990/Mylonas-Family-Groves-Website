e6daaec0d95f1a23613300433237c5e52975b10a
GeoMil <geo_mil@windowslive.com>
2022-02-17 21:45:02 +0200

    gallery pop-up image (js)

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,10 +1,15 @@
 const gallery = [].slice.call(document.querySelectorAll(".gallery-item"));
+let currentImage;
+const arrowNext = document.getElementById("arrow-next");
+const arrowPrevious = document.getElementById("arrow-previous");
 
 const showPhoto = (e) => {
   //Hide the gallery
   e.path[1].style.display = "none";
-  console.log(e);
+
+  //Show the popup Box
   const galleryPopUp = document.getElementById("gallery-container-popup");
+  /*  galleryPopUp.style.display = "block"; */
   galleryPopUp.style.visibility = "visible";
   galleryPopUp.style.opacity = "1";
   galleryPopUp.scrollIntoView({
@@ -12,7 +17,18 @@ const showPhoto = (e) => {
     block: "center",
     inline: "center",
   });
-  console.log(galleryPopUp);
+
+  //Show the pressed image
+  currentImage = e.target.id;
+  pressNext();
+  e.target.src = currentImage;
+  console.log(e.target.src);
 };
+
+//Show Next Image
+const pressNext = () => ++currentImage;
+
+//Show Previous Image
+const pressPrevious = () => {};
 
 gallery.forEach((image) => image.addEventListener("click", showPhoto));
