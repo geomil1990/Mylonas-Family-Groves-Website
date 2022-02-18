4ded8162ee5b528b6b0de41d2ba92c4e276c8477
GeoMil <geo_mil@windowslive.com>
2022-02-19 00:24:21 +0200

    playIcon changes regarding the state (play or pause)

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,17 +3,19 @@ let currentImageID;
 const arrowNext = document.getElementById("arrow-next");
 const arrowPrevious = document.getElementById("arrow-previous");
 const playBtn = document.getElementById("play");
-const pause = document.getElementById("pause");
+/* const pauseBtn = document.getElementById("pause"); */
+const stopBtn = document.getElementById("stop");
 const ppcBtns = document.getElementById("play-pause-cancel");
 const galleryPopUp = document.getElementById("gallery-container-popup");
 const gallery = document.getElementById("gallery-container");
+let playIcon = document.querySelector("#play img");
+let pauseicon = document.querySelector("#pause img");
 let myInterval;
 
 const showPhoto = (e) => {
   //Hide the gallery
   gallery.style.display = "none";
   console.log(gallery);
-
   //Display the popup Box at the center of the view
   /*  galleryPopUp.style.display = "block"; */
   galleryPopUp.style.visibility = "visible";
@@ -27,13 +29,10 @@ const showPhoto = (e) => {
   //------------------ Show the pressed image ------------------
   //get the element img from #gallery-image-popup
   popUpImage = document.querySelector("#gallery-image-popup img");
-
   //set currentImageID with the id of the selected image
   currentImageID = e.target.querySelector("img").id;
-
   //change the src of the image that is displayed with the selected one
   popUpImage.src = "../img/" + currentImageID + ".jpg";
-
   //Display cancel Button
   ppcBtns.style.visibility = "visible";
 };
@@ -43,10 +42,8 @@ const pressNext = () => {
   if (currentImageID < 9) ++currentImageID;
   else currentImageID = 1;
   console.log("Next");
-
   //get the element img from #gallery-image-popup
   popUpImage = document.querySelector("#gallery-image-popup img");
-
   //change the src of the image that is displayed with the selected one
   popUpImage.src = "../img/" + currentImageID + ".jpg";
   console.log(popUpImage);
@@ -56,10 +53,8 @@ const pressNext = () => {
 const pressPrevious = () => {
   if (currentImageID > 1) --currentImageID;
   else currentImageID = 9;
-
   //get the element img from #gallery-image-popup
   popUpImage = document.querySelector("#gallery-image-popup img");
-
   //change the src of the image that is displayed with the selected one
   popUpImage.src = "../img/" + currentImageID + ".jpg";
   console.log(popUpImage);
@@ -68,29 +63,57 @@ const pressPrevious = () => {
 
 //Play
 const play = () => {
-  myInterval = setInterval(() => {
-    /* while (currentImageID) { */
-    //get the element img from #gallery-image-popup
-    popUpImage = document.querySelector("#gallery-image-popup img");
+  clearInterval(myInterval);
+  //Use the alt attribute in order to check the state of the button
+  if (playIcon.alt === "play") {
+    //change the image and the attribute alt of the button
+    playIcon.src = "../img/pause-24px.png";
+    playIcon.alt = "pause";
+    myInterval = setInterval(() => {
+      //get the element img from #gallery-image-popup
+      popUpImage = document.querySelector("#gallery-image-popup img");
+      //change the src of the image that is displayed with the selected one
+      popUpImage.src = "../img/" + currentImageID + ".jpg";
+      ++currentImageID;
+      if (currentImageID >= 10) currentImageID = 1;
+      console.log(currentImageID);
+      console.log(playIcon.alt);
+    }, 3000);
+  } else if (playIcon.alt === "pause") {
+    //change the image and the attribute alt of the button
+    playIcon.src = "../img/play-24px.png";
+    playIcon.alt = "play";
+    //clear the timer
+    clearInterval(myInterval);
+    console.log(playIcon.alt);
+  }
+};
 
-    //change the src of the image that is displayed with the selected one
-    popUpImage.src = "../img/" + currentImageID + ".jpg";
-    ++currentImageID;
-    if (currentImageID >= 10) currentImageID = 1;
-    console.log(currentImageID);
-    /*  } */
-  }, 3000);
+//Pause
+const pause = () => {
+  clearInterval(myInterval);
+};
+
+//Stop
+const stop = () => {
+  //clear Interval in order to stop the timer
+  clearInterval(myInterval);
+  //Show the first image
+  currentImageID = 1;
+  //get the element img from #gallery-image-popup
+  popUpImage = document.querySelector("#gallery-image-popup img");
+  //change the src of the image that is displayed with the selected one
+  popUpImage.src = "../img/" + currentImageID + ".jpg";
 };
 
 //Cancel and return to the Gallery
 const goBack = () => {
-  //clear Interval in order to stop the timer and close the window
+  //clear Interval in order to stop the timer
   clearInterval(myInterval);
   currentImageID = 0;
   //Hide the popup Box
   galleryPopUp.style.visibility = "hidden";
   galleryPopUp.style.opacity = "0";
-
   //Display the gallery at the center of the view
   /*  galleryPopUp.style.display = "block"; */
   gallery.style.display = "grid";
@@ -99,8 +122,7 @@ const goBack = () => {
     block: "center",
     inline: "center",
   });
-
-  //Stop Displaying cancel Button
+  //Stop Displaying all the buttons
   ppcBtns.style.visibility = "hidden";
 };
 
@@ -108,4 +130,6 @@ images.forEach((image) => image.addEventListener("click", showPhoto));
 arrowNext.addEventListener("click", pressNext);
 arrowPrevious.addEventListener("click", pressPrevious);
 playBtn.addEventListener("click", play);
+/* pauseBtn.addEventListener("click", pause); */
+stopBtn.addEventListener("click", stop);
 cancel.addEventListener("click", goBack);
