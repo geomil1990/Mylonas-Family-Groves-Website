a85cd34209aa1ee88f36b41e2e99c3d38ee5a161
GeoMil <geo_mil@windowslive.com>
2022-02-18 18:01:31 +0200

    Press Next & Previous at Gallery created

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,4 +1,4 @@
-const gallery = [].slice.call(document.querySelectorAll(".gallery-item"));
+const images = [].slice.call(document.querySelectorAll(".gallery-item"));
 let currentImage;
 const arrowNext = document.getElementById("arrow-next");
 const arrowPrevious = document.getElementById("arrow-previous");
@@ -7,7 +7,7 @@ const showPhoto = (e) => {
   //Hide the gallery
   e.path[1].style.display = "none";
 
-  //Show the popup Box
+  //Display the popup Box at the center of the view
   const galleryPopUp = document.getElementById("gallery-container-popup");
   /*  galleryPopUp.style.display = "block"; */
   galleryPopUp.style.visibility = "visible";
@@ -18,17 +18,41 @@ const showPhoto = (e) => {
     inline: "center",
   });
 
-  //Show the pressed image
-  currentImage = e.target.id;
-  pressNext();
-  e.target.src = currentImage;
-  console.log(e.target.src);
+  //------------------ Show the pressed image ------------------
+  //get the element img from #gallery-image-popup
+  popUpImage = document.querySelector("#gallery-image-popup img");
+
+  //set currentImageID with the id of the selected image
+  currentImageID = e.target.querySelector("img").id;
+
+  //change the src of the image that is displayed with the selected one
+  popUpImage.src = "../img/" + currentImageID + ".jpg";
+  console.log(popUpImage);
 };
 
 //Show Next Image
-const pressNext = () => ++currentImage;
+const pressNext = () => {
+  if (currentImageID < 9) ++currentImageID;
+  else currentImageID = 1;
+  console.log("Next");
+  //get the element img from #gallery-image-popup
+  popUpImage = document.querySelector("#gallery-image-popup img");
+  //change the src of the image that is displayed with the selected one
+  popUpImage.src = "../img/" + currentImageID + ".jpg";
+  console.log(popUpImage);
+};
 
 //Show Previous Image
-const pressPrevious = () => {};
+const pressPrevious = () => {
+  --currentImageID;
+  //get the element img from #gallery-image-popup
+  popUpImage = document.querySelector("#gallery-image-popup img");
+  //change the src of the image that is displayed with the selected one
+  popUpImage.src = "../img/" + currentImageID + ".jpg";
+  console.log(popUpImage);
+  console.log("Previous");
+};
 
-gallery.forEach((image) => image.addEventListener("click", showPhoto));
+images.forEach((image) => image.addEventListener("click", showPhoto));
+arrowNext.addEventListener("click", pressNext);
+arrowPrevious.addEventListener("click", pressPrevious);
